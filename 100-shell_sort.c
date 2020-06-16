182621ce54147cd103ffe404c32865fef8bb4a8f
Camilo tobon <1611@holbertonschool.com>
2020-06-16 13:52:46 -0500

    addd

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,7 +1,6 @@
 #include "sort.h"
 
 /**
-<<<<<<< HEAD
  * knuth- check interval and sort array
  * @array: The array that we wants to sort
  * @size: The size of the array
@@ -46,39 +45,3 @@ void shell_sort(int *array, size_t size)
 		;
 	knuth(array, iterator, size);
 }
-=======
-* shell_sort - sholl sort algorithm implemented
-* @array: the array
-* @size: size of the array
-*/
-
-void shell_sort(int *array, size_t size)
-{
-	size_t i = 0, interval = 1, val_aux = 0;
-	int va = 0;
-
-	if (!array || size < 2)
-		return;
-
-	while (interval <= size / 3)
-		interval = interval * 3 + 1;
-
-	while (interval > 0)
-	{
-		for (i = interval; i < size; i++)
-		{
-			val_aux = i;
-			va = array[i];
-
-			while (val_aux > interval - 1 && array[val_aux - interval] > va)
-			{
-				array[val_aux] = array[val_aux - interval];
-				val_aux -= interval;
-			}
-			array[val_aux] = va;
-		}
-		print_array(array, size);
-		interval = (interval - 1) / 3;
-	}
-}
->>>>>>> e2694305bc5d2e6cff88095a2f3160dfb7e57d68
