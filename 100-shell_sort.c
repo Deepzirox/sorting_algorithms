c0a7bb20fd7b484ef42ddcdf500cdb37d4345fd4
Blazeker <1687@holbertonschool.com>
2020-06-16 18:32:57 +0000

    Advanced task

---
+++ 100-shell_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+* shell_sort - sholl sort algorithm implemented
+* @array: the array
+* @size: size of the array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	size_t i = 0, interval = 1, val_aux = 0;
+	int va = 0;
+
+    if (!array || size < 2)
+        return;
+
+    while (interval <= size / 3)
+        interval = interval * 3 + 1;
+
+    while (interval > 0)
+    {
+        for (i = interval; i < size; i++)
+        {
+            val_aux = i;
+            va = array[i];
+
+            while (val_aux > interval - 1 && array[val_aux - interval] > va)
+            {
+                array[val_aux] = array[val_aux - interval];
+                val_aux -= interval;
+            }
+            array[val_aux] = va;
+        }
+        print_array(array, size);
+        interval = (interval - 1) / 3;
+    }
+}
