9d80a6c47abb95a0ba83969d0b5bd138f46279df
Blazeker <1687@holbertonschool.com>
2020-06-16 18:37:28 +0000

    Betty fixed

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,27 +11,27 @@ void shell_sort(int *array, size_t size)
 	size_t i = 0, interval = 1, val_aux = 0;
 	int va = 0;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    while (interval <= size / 3)
-        interval = interval * 3 + 1;
+	while (interval <= size / 3)
+		interval = interval * 3 + 1;
 
-    while (interval > 0)
-    {
-        for (i = interval; i < size; i++)
-        {
-            val_aux = i;
-            va = array[i];
+	while (interval > 0)
+	{
+		for (i = interval; i < size; i++)
+		{
+			val_aux = i;
+			va = array[i];
 
-            while (val_aux > interval - 1 && array[val_aux - interval] > va)
-            {
-                array[val_aux] = array[val_aux - interval];
-                val_aux -= interval;
-            }
-            array[val_aux] = va;
-        }
-        print_array(array, size);
-        interval = (interval - 1) / 3;
-    }
+			while (val_aux > interval - 1 && array[val_aux - interval] > va)
+			{
+				array[val_aux] = array[val_aux - interval];
+				val_aux -= interval;
+			}
+			array[val_aux] = va;
+		}
+		print_array(array, size);
+		interval = (interval - 1) / 3;
+	}
 }
